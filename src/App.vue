6a4137b8a7f8fcf6eff41e7d679329e6196ede4e
Anand Chowdhary <mail@anandchowdhary.com>
2019-06-03 16:34:16 +0200

    :tada: Initialize repo

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <nav id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <main><router-view /></main>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  font: menu;
+  font-size: 100%;
+  margin: 0;
+}
+</style>
+
+<style lang="scss" scoped>
+nav {
+  position: fixed;
+  overflow: auto;
+  background-color: #eee;
+}
+main {
+  margin: auto;
+  padding: 2rem;
+  max-width: 600px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>Hello</h1>
+  </div>
+</template>
